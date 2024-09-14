7e9eea4fb1145f93354dd89c595639eb70bfead9
Vivek Manavalan <vivekmanavalan0@gmail.com>
2024-09-14 12:44:16 +0800

    refresh cells when updated

--- src/app/ag-table/ag-table.component.ts
+++ src/app/ag-table/ag-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ColDef, ModuleRegistry } from 'ag-grid-community';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-quartz.css';
@@ -6,11 +6,13 @@ import 'ag-grid-community/styles/ag-theme-alpine.css';
 import { ToggleRendererComponent } from '../toggle-renderer/toggle-renderer.component';
 import { DialogComponent } from '../dialog/dialog.component';
 import { MatDialog } from '@angular/material/dialog';
+import { AgGridAngular } from 'ag-grid-angular';
 
 interface IRow {
   make: string;
   price: number;
   electric: boolean;
+  lastModified: Date;
 }
 
 @Component({
@@ -23,19 +25,30 @@ export class AgTableComponent implements OnInit {
   originalData: any[] = [];
   constructor(public dialog: MatDialog) {}
   themeClass = "ag-theme-quartz";
+  @ViewChild('agGrid')
+  agGrid!: AgGridAngular;
+
+  getYesterdayDate(): Date {
+    const today = new Date();
+    const yesterday = new Date(today);
+    yesterday.setDate(today.getDate() - 1);
+    return yesterday;
+  }
 
   // Row Data: The data to be displayed.
   rowData: IRow[] = [
-      { make: 'Tesla',  price: 64950, electric: true },
-      { make: 'Ford',  price: 33850, electric: false },
-      { make: 'Toyota', price: 29600, electric: false },
-      { make: 'Mercedes',  price: 48890, electric: true },
-      { make: 'Fiat',  price: 15774, electric: false },
-      { make: 'Nissan', price: 20675, electric: false },
+      { make: 'Tesla',  price: 64950, electric: true, lastModified: this.getYesterdayDate() },
+      { make: 'Ford',  price: 33850, electric: false, lastModified: this.getYesterdayDate() },
+      { make: 'Toyota', price: 29600, electric: false, lastModified: this.getYesterdayDate() },
+      { make: 'Mercedes',  price: 48890, electric: true, lastModified: this.getYesterdayDate() },
+      { make: 'Fiat',  price: 15774, electric: false, lastModified: this.getYesterdayDate() },
+      { make: 'Nissan', price: 20675, electric: false, lastModified: this.getYesterdayDate() },
   ];
 
   // Column Definitions: Defines & controls grid columns.
-  colDefs: ColDef<IRow>[] = [{ field: 'make' }, { field: 'price' }, { field: 'electric', headerName: 'Access', cellRenderer: 'toggleRenderer' }];
+  colDefs: ColDef<IRow>[] = [{ field: 'make' }, { field: 'price' }, 
+    { field: 'electric', headerName: 'Access', cellRenderer: 'toggleRenderer' }, 
+    { field: 'lastModified' }];
 
   defaultColDef: ColDef = {
       flex: 1,
@@ -56,12 +69,20 @@ export class AgTableComponent implements OnInit {
       data: { changes }
     });
 
+    this.rowData.forEach((row, index) => {
+      const originalRow = this.originalData[index];
+      if (JSON.stringify(row) !== JSON.stringify(originalRow)) {
+        row.lastModified = new Date();
+      }
+    });
+
     dialogRef.afterClosed().subscribe(result => {
       console.log(result);
       if (result) {
         console.log("Saving changes", this.rowData);
       }
     });
+    this.agGrid.api.refreshCells({ force: true });
   }
 
   getChanges(): string[] {
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgTableComponent } from './ag-table/ag-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'access', component: AgTableComponent },
+  // { path: '', redirectTo: '/access', pathMatch: 'full' }, // Default route
+  // { path: '**', redirectTo: '/access' } // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
